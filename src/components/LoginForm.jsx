af29956a16dbbbffe5303266abf5fdde39a898cc
Sam Esserman <sesserman97@gmail.com>
2023-07-10 12:03:58 -0400

    completed loginform

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,9 @@
+const LoginForm = () => {
+  // Add login form logic here
+  return (
+    <div>
+      <h2>Login</h2>
+      {/* Add login form fields and submit button here */}
+    </div>
+  )
+}
