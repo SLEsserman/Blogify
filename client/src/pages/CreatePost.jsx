8c7d5473c0b8c3f9149af2a3ee55ad0f77828715
Sam Esserman <sesserman97@gmail.com>
2023-07-18 17:43:04 -0400

    frontend for the category options are working

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,26 +1,39 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect } from "react"
 import Header from "../components/Header"
 import { Context } from "../context"
 import { Link } from "react-router-dom"
-import { useState } from "react";
-import axios from 'axios';
+import { useState } from "react"
+import axios from "axios"
 
 function CreatePost() {
-  const { state } = useContext(Context);
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const { state } = useContext(Context)
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/category/all")
+        setCategories(response.data.categories)
+      } catch (err) {
+        setCategories([])
+      }
+    }
+    fetchCategories()
+  }, [])
 
   const handleCreatePost = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-        const response = await axios.post('http://localhost:3000/blog/new', {
+      const response = await axios.post("http://localhost:3000/blog/new", {
         title,
         content,
       })
-      alert('Blog post made successfully');
+      alert("Blog post made successfully")
       //console.log(response);
     } catch (err) {
-      alert('Unable to make a post', err)
+      alert("Unable to make a post", err)
     }
   }
 
@@ -30,28 +43,57 @@ function CreatePost() {
       <div className="bg-blue-400 p-4 h-screen">
         <h1 className="text-center font-bold text-2xl pt-4">Profile Page</h1>
         <div className="grid grid-cols-2 gap-4 mt-4">
-          <div className="text-xl"><b>Name:</b> {state?.user?.name}</div>
-          <div className="text-xl"><b>Email:</b> {state?.user?.email}</div>
+          <div className="text-xl">
+            <b>Name:</b> {state?.user?.name}
+          </div>
+          <div className="text-xl">
+            <b>Email:</b> {state?.user?.email}
+          </div>
         </div>
         <Link to="/">
-          <button className="bg-white text-blue-700 rounded-md p-2 mt-4">Go back Home</button>
+          <button className="bg-white text-blue-700 rounded-md p-2 mt-4">
+            Go back Home
+          </button>
         </Link>
         <div className="mx-80 bg-white p-4">
           <form onSubmit={handleCreatePost}>
             <div className="grid grid-cols-1 mt-4">
               <div>
                 <p className="mb-2 font-bold text-md">Post Title</p>
-                <input className="w-full h-12 border border-gray-200 rounded-md px-2" type="text" placeholder="Post Title" onChange={(e) => setTitle(e.target.value)} />
+                <input
+                  className="w-full h-12 border border-gray-200 rounded-md px-2"
+                  type="text"
+                  placeholder="Post Title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-1 mt-4">
+              <div>
+                <p className="mb-2 font-bold text-md">Post Category</p>
+                <select className="w-full h-12 border border-gray-200 rounded-md px-2">
+                  {categories?.map((category) => {
+                    return (
+                      <option key={category._id} value={category._id}>{category.name}</option>
+                    )
+                  })}
+                </select>
               </div>
             </div>
             <div className="grid grid-cols-1 mt-4">
               <div>
                 <p className="mb-2 font-bold text-md">Post Body</p>
-                <textarea className="w-full border border-gray-200 rounded-md px-2" rows={10} onChange={(e) => setContent(e.target.value)}></textarea>
+                <textarea
+                  className="w-full border border-gray-200 rounded-md px-2"
+                  rows={10}
+                  onChange={(e) => setContent(e.target.value)}
+                ></textarea>
               </div>
             </div>
             <div>
-              <button className="w-full bg-blue-400 text-white p-2 rounded-md mt-4">Create Post</button>
+              <button className="w-full bg-blue-400 text-white p-2 rounded-md mt-4">
+                Create Post
+              </button>
             </div>
           </form>
         </div>
