46aec99949682702de97fc1a16592d61958948d4
Sam Esserman <sesserman97@gmail.com>
2023-07-18 11:53:29 -0400

    more clean up

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -11,12 +11,6 @@ const Sidebar = () => {
         <li className="bg-blue-300 p-2 m-2 rounded-md text-sm font-bold">
           <Link to="/profile">Profile</Link>
         </li>
-        <li className="bg-blue-300 p-2 m-2 rounded-md text-sm font-bold">
-          <Link to="/explore">Explore</Link>
-        </li>
-        <li className="bg-blue-300 p-2 m-2 rounded-md text-sm font-bold">
-          <Link to="/recommended">Recommended</Link>
-        </li>
       </ul>
     </nav>
   )
