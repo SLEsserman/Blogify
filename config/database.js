06152cb4974b12f84b199ce9516f093ebfeecb6c
Sam Esserman <sesserman97@gmail.com>
2023-07-18 19:35:30 -0400

    added to the database file

--- config/database.js
+++ config/database.js
@@ -1,13 +1,30 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// Connect to the MongoDB database using the provided DATABASE_URL environment variable
-mongoose.connect(process.env.DATABASE_URL);
+// // Connect to the MongoDB database using the provided DATABASE_URL environment variable
+// mongoose.connect(process.env.DATABASE_URL);
 
-// Shortcut to the mongoose.connection object
-const db = mongoose.connection;
+// // Shortcut to the mongoose.connection object
+// const db = mongoose.connection;
 
-// Event listener for the 'connected' event, triggered when the connection to MongoDB is established
-db.on('connected', function() {
-  // Prints a message of successful connection- the database name, host, and port
-  console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
-});
+// // Event listener for the 'connected' event, triggered when the connection to MongoDB is established
+// db.on('connected', function() {
+//   // Prints a message of successful connection- the database name, host, and port
+//   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+// });
+
+
+const mongoose = require('mongoose')
+require('dotenv').config() // Add this line if you have not already
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Successfully connected to MongoDB!')
+  })
+  .catch((e) => {
+    console.error('Connection error', e.message)
+  })
+mongoose.set('debug', true)
+const db = mongoose.connection
+
+module.exports = db
